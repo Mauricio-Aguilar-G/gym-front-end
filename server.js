ce691b71e5307dd0e15be20d54d1f9ed369d39a9
Mauricio-Aguilar-G <mv.aguilargarcia@ugto.mx>
2022-06-13 18:27:02 -0500

    front second commit

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const serveStatic = require('serve-static')
-const path = require('path')
+const path = require('path');
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname,'/dist')))//dist is created with npm run build
+app.use('/', serveStatic(path.join(__dirname,'/dist/')));//dist is created with npm run build
+app.get(/.*/, function(req, res) {
+    res.sendFile(__dirname + "/dist/index.html");
+});
 
-const port = process.env.PORT || 8080
-app.listen(port)
+const port = process.env.PORT || 8080;
+app.listen(port);
 
-console.log('listening on port: '+port)
+console.log('listening on port: '+port);
